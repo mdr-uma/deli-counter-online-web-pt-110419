96d10070d40872c3677dbf9f257b8423f4b9b41e
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-12 19:13:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,36 @@
 # Write your code here.
-def line(katz_deli)
-  puts "The line is currently empty."
+katz_deli = []
 
+def line(katz_deli)
+  array = [];
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      array << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{array.join(" ")}"
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# def line(katz_deli)
+#   puts "The line is currently empty."
+# end
+# 
+# def take_a_number(array, string)
+#   
+#   
+# end
