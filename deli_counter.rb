733e17a3c4ef2c05eccc780913a88f820367fa0d
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-12 19:15:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,21 +16,6 @@ def line(katz_deli)
 end
 
 
+def take_a_number(array, string)
 
-
-
-
-
-
-
-
-
-
-# def line(katz_deli)
-#   puts "The line is currently empty."
-# end
-#
-# def take_a_number(array, string)
-#
-#
-# end
+end
