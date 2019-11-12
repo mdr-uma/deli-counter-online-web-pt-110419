37fb7b160e4258be3d3bd876f2d69196cb6cf0ca
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-12 19:20:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ def line(katz_deli)
 end
 
 
-def take_a_number(array, string)
-
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
